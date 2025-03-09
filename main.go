f79f83986bad1b5703c0de2c32d9312ea7642468
vinay-kumar-ps <vinaykumarps321@gmail.com>
2025-03-09 23:15:43 +0530

    learn something new

--- main.go
+++ main.go
@@ -2,42 +2,78 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 )
+
+var ctx = context.Background()
+
+type Person struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	Age        int    `json:"age"`
+	Occupation string `json:"occupation"`
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 
 	client := redis.NewClient(&redis.Options{
-Addr: "localhost:6379",
-Password: "",
-DB: 0,
-})
-ping,err := client.Ping(context.Background()).Result()
-if err!=nil {
-	fmt.Println(err.Error())
-	return
-}
-fmt.Println("Ping response:",ping)
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
 
-type person struct{
-	Name string `json:"name"`
-	Age int `json:"age"`
-	occupation string `json:"occupation"`
-}
+	// Ping Redis server
+	if _, err := client.Ping(ctx).Result(); err != nil {
+		fmt.Println("Error connecting to Redis:", err)
+		return
+	}
+	fmt.Println("Connected to Redis successfully")
 
-jsonstring := `{"name":"moneypenny","age":24,"occupation":"programmer"}`
+	// Create a new person
+	eID := uuid.NewString()
+	person := Person{
+		ID:         eID,
+		Name:       "Moneypenny",
+		Age:        24,
+		Occupation: "Programmer",
+	}
 
-err=client.Set(context.Background(),"person",jsonstring,0).Err()
-if err!= nil{
-	fmt.Println(err.Error())
-	return
-}
-val,err :=client.Get(context.Background(),"person").Result()
-if err!= nil{
-	fmt.Println("Error getting value",err.Error())
-	return
-}
-fmt.Println("name retrived from redis",val)
+	// Serialize person to JSON
+	jsonString, err := json.Marshal(person)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	// Store JSON in Redis
+	err = client.Set(ctx, eID, jsonString, 0).Err()
+	if err != nil {
+		fmt.Println("Error saving to Redis:", err)
+		return
+	}
+
+	fmt.Println("Person saved successfully with ID:", eID)
+
+	// Retrieve JSON from Redis
+	val, err := client.Get(ctx, eID).Result()
+	if err != nil {
+		fmt.Println("Error retrieving from Redis:", err)
+		return
+	}
+
+	fmt.Println("Data retrieved from Redis:", val)
+
+	// Deserialize JSON back to struct
+	var retrievedPerson Person
+	if err := json.Unmarshal([]byte(val), &retrievedPerson); err != nil {
+		fmt.Println("Error unmarshaling JSON:", err)
+		return
+	}
+
+	fmt.Printf("Deserialized Person: %+v\n", retrievedPerson)
 }
